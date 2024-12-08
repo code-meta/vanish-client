0b606f6e5286fded2f10766ade195dbaf167a0b9
code-meta <fahim.x@yahoo.com>
2024-12-09 00:03:31 +0600

    feat: tweak text message card css

--- app/chat/direct/_components/TextMessageCard.tsx
+++ app/chat/direct/_components/TextMessageCard.tsx
@@ -1,11 +1,20 @@
+import { useAppSelector } from "@/lib/hooks";
 import { Message, TextMessage } from "@/lib/types";
+import { cn } from "@/lib/utils";
 import React from "react";
 
-const TextMessageCard = ({ messagePayload }: Message) => {
+const TextMessageCard = ({ messagePayload, creator_id }: Message) => {
+  const user = useAppSelector((state) => state.user.user);
+
   if (messagePayload.type !== "TEXT") return null;
 
   return (
-    <div className="bg-card bg-gradient-to-bl from-card to-primary/15 text-card-foreground px-4 max-w-[90%] md:max-w-[50%] inline-flex items-center justify-center py-2 rounded-2xl">
+    <div
+      className={cn(
+        "bg-card from-card to-primary/25 text-card-foreground px-4 max-w-[90%] md:max-w-[50%] inline-flex items-center justify-center py-2 rounded-2xl",
+        user.id === creator_id ? "bg-gradient-to-br" : "bg-gradient-to-bl"
+      )}
+    >
       <p className="">{messagePayload.content}</p>
     </div>
   );
